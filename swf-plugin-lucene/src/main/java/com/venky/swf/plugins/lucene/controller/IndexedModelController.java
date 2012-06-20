ab9cd52c520b52ad4e53e1ae134948f71ea889a4
Venky <venkatramnm@gmail.com>
2012-06-21 00:28:07 +0530

    Fixed Empty Search Query Exception

--- swf-plugin-lucene/src/main/java/com/venky/swf/plugins/lucene/controller/IndexedModelController.java
+++ swf-plugin-lucene/src/main/java/com/venky/swf/plugins/lucene/controller/IndexedModelController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.apache.lucene.search.Query;
 
 import com.venky.core.string.StringUtil;
+import com.venky.core.util.ObjectUtil;
 import com.venky.swf.controller.ModelController;
 import com.venky.swf.db.model.Model;
 import com.venky.swf.path.Path;
@@ -68,13 +69,17 @@ public class IndexedModelController<M extends Model>  extends ModelController<M>
 	}
 	
 	protected List<M> search(Map<String,Object> formData){
-		LuceneIndexer indexer = LuceneIndexer.instance(getModelClass());
 		String strQuery = StringUtil.valueOf(formData.get("q"));
-		
-		Query q = indexer.constructQuery(strQuery);
-		List<Integer> ids = indexer.findIds(q, 100);
-		Select sel = new Select().from(getModelClass()).where(new Expression("ID",Operator.IN,ids.toArray()));
-		return sel.execute(getModelClass());
+		if (!ObjectUtil.isVoid(strQuery)){
+			LuceneIndexer indexer = LuceneIndexer.instance(getModelClass());
+			Query q = indexer.constructQuery(strQuery);
+			List<Integer> ids = indexer.findIds(q, 100);
+			if (!ids.isEmpty()) {
+				Select sel = new Select().from(getModelClass()).where(new Expression("ID",Operator.IN,ids.toArray()));
+				return sel.execute(getModelClass());
+			}
+		}
+		return new ArrayList<M>();
 	}
 
 	protected void rewriteQuery(Map<String,Object> formData){
