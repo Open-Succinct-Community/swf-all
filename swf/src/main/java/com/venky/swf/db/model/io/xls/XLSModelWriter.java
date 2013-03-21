80cd2726f1d50af744f744feb03a555e43aa9c76
Venky <venkatramnm@gmail.com>
2013-03-21 13:11:15 +0530

    Courier New Changes

--- swf/src/main/java/com/venky/swf/db/model/io/xls/XLSModelWriter.java
+++ swf/src/main/java/com/venky/swf/db/model/io/xls/XLSModelWriter.java
@@ -3,19 +3,25 @@ package com.venky.swf.db.model.io.xls;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 
 import com.venky.core.collections.SequenceSet;
 import com.venky.core.string.StringUtil;
@@ -72,14 +78,16 @@ public class XLSModelWriter<M extends Model> extends XLSModelIO<M> implements Mo
 		wb.write(os);
 	}
 	public void write(List<M> records, Workbook wb, List<String> fields) {
+		Font font = createDefaultFont(wb);
+		
 		CellStyle headerStyle = wb.createCellStyle();
 		headerStyle.setFillForegroundColor(IndexedColors.SKY_BLUE.getIndex());
 		headerStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
-
+		headerStyle.setFont(font);
 		
 		String sheetName = StringUtil.pluralize(getBeanClass().getSimpleName());
 		Sheet sheet = wb.createSheet(sheetName);
-
+		
     	Bucket rowNum = new Bucket(START_ROW); 
     	Bucket columnNum = new Bucket(START_COLUMN);
     	Row header = sheet.createRow(rowNum.intValue());
@@ -88,16 +96,10 @@ public class XLSModelWriter<M extends Model> extends XLSModelIO<M> implements Mo
 		while (fi.hasNext()){
 			String fieldName = fi.next();
 			if (referedModelMap.get(fieldName) == null){
-				Cell headerCell = header.createCell(columnNum.intValue());
-				headerCell.setCellStyle(headerStyle);
-				headerCell.setCellValue(StringUtil.camelize(fieldName));
-				columnNum.increment();
+				createCell(sheet, header, columnNum, StringUtil.camelize(fieldName), headerStyle);
 			}else {
 				for (String headerField : referredModelFieldsToExport.get(fieldName)){
-					Cell headerCell = header.createCell(columnNum.intValue());
-					headerCell.setCellStyle(headerStyle);
-					headerCell.setCellValue(headerField);
-					columnNum.increment();
+					createCell(sheet, header, columnNum, StringUtil.camelize(headerField), headerStyle);
 				}
 			}
 		}
@@ -125,32 +127,35 @@ public class XLSModelWriter<M extends Model> extends XLSModelIO<M> implements Mo
 	    			m = clone;
 	    		}
     		}
-    		write(m,r,fields);
+    		write(m,r,font,fields);
     	}
     	
-    	for (int i = 0 ; i < columnNum.intValue() ; i ++ ){
-    		sheet.autoSizeColumn(i);
-    		sheet.setColumnWidth(i, Math.max(Math.min(sheet.getColumnWidth(i), 40 * 256),5 * 256));
-    	}
 	}
 
 	@Override
 	public void write(M m, Row r, List<String> fields) {
+		write(m,r,null,fields);
+	}
+	private void write(M m, Row r, Font font, List<String> fields) {
 		Workbook wb = r.getSheet().getWorkbook();
 		
 		CreationHelper createHelper = wb.getCreationHelper();
 		CellStyle decimalStyle = wb.createCellStyle();
 		decimalStyle.setDataFormat(createHelper.createDataFormat().getFormat("#.0##"));
+		decimalStyle.setFont(font);
 		
 		CellStyle integerStyle = wb.createCellStyle();
 		integerStyle.setDataFormat(createHelper.createDataFormat().getFormat("0"));
+		integerStyle.setFont(font);
 		
 		CellStyle dateStyle = wb.createCellStyle();
 		dateStyle.setDataFormat(createHelper.createDataFormat().getFormat("d/m/yyyy"));
+		dateStyle.setFont(font);
 		
 		CellStyle stringStyle = wb.createCellStyle();
 		stringStyle.setWrapText(true);
-	
+		stringStyle.setFont(font);
+		
 		Iterator<String> fi = fields.iterator();
 		ModelReflector<M> ref = getReflector();
 		Bucket columnNum = new Bucket(START_COLUMN);
@@ -160,39 +165,123 @@ public class XLSModelWriter<M extends Model> extends XLSModelIO<M> implements Mo
 			if (referedModelMap.get(f) != null){
 				for (String cf: referredModelFieldsToExport.get(f) ){
 					writeNextColumn(r, columnNum, getValue(m,cf), integerStyle, decimalStyle,dateStyle,stringStyle);
-					columnNum.increment();
 				}
 			}else {
 				writeNextColumn(r, columnNum, value, integerStyle,decimalStyle, dateStyle,stringStyle);
-				columnNum.increment();
 			}
 		}
 	}
 	
 
 	
-	protected void writeNextColumn(Row r, Bucket columnNum , Object value, CellStyle integerStyle, CellStyle decimalStyle,CellStyle dateStyle,CellStyle stringStyle){
+	protected Cell writeNextColumn(Row r, Bucket columnNum , Object value, CellStyle integerStyle, CellStyle decimalStyle,CellStyle dateStyle,CellStyle stringStyle){
+		Cell cell = null; 
 		if (!ObjectUtil.isVoid(value)){
 			Class<?> colClass = value.getClass();
-			Cell cell = r.createCell(columnNum.intValue());
 			if (isNumeric(colClass)){
-                cell.setCellValue(Double.valueOf(String.valueOf(value)));
-                
 				if (NumericConverter.class.isAssignableFrom(Database.getJdbcTypeHelper().getTypeRef(colClass).getTypeConverter().getClass())){
-					cell.setCellStyle(decimalStyle);	
+					cell = createCell(r.getSheet(),r,columnNum,value,decimalStyle);
 				}else {
-					cell.setCellStyle(integerStyle);
+					cell = createCell(r.getSheet(),r,columnNum,value,integerStyle);
 				}
             }else if (isDate(colClass)) {
-                cell.setCellValue((Date)value);
-                cell.setCellStyle(dateStyle);
+            	cell = createCell(r.getSheet(), r, columnNum, value, dateStyle);
             }else if (isBoolean(colClass)) {
-                cell.setCellValue((Boolean)value);
+                cell = createCell(r.getSheet(), r, columnNum, value, null);
             }else{
-                cell.setCellValue(Database.getJdbcTypeHelper().getTypeRef(colClass).getTypeConverter().toString(value));
-                cell.setCellStyle(stringStyle);
+                cell = createCell(r.getSheet(),r , columnNum, value , stringStyle);
             }
+			
 		}
+		return cell;
 	}
 
+	public static final int MAX_COLUMN_LENGTH = 50 ;
+	public static final int CHARACTER_WIDTH = 293;
+	public static final int CHARACTER_HEIGHT_IN_POINTS = 12 ;
+	
+	private int getColumnWidth(Sheet sheet,int columnIndex){
+		if (sheet.getNumMergedRegions() == 0){
+			return sheet.getColumnWidth(columnIndex);
+		}else {
+			boolean columnMergedInSomeRow = false; 
+			Set<Integer> skipRows = new HashSet<Integer>();
+			for (int i = 0 ; i < sheet.getNumMergedRegions() ; i ++ ){
+				CellRangeAddress add = sheet.getMergedRegion(i);
+				if (add.getFirstColumn() <= columnIndex && columnIndex <= add.getLastColumn() && (add.getFirstColumn() != add.getLastColumn()) ){
+					columnMergedInSomeRow = true;
+					for (int r = add.getFirstRow() ; r <= add.getLastRow() ; r ++ ){
+						skipRows.add(r);
+					}
+				}
+			}
+			if (!columnMergedInSomeRow){
+				return sheet.getColumnWidth(columnIndex);
+			}else {
+				int maxSkippedRow = Collections.max(skipRows);
+				if (sheet.getPhysicalNumberOfRows() -1 <= maxSkippedRow + 1){
+					return 0;
+				}else {
+					return sheet.getColumnWidth(columnIndex);	
+				}
+			}
+		}
+	}
+	private int getRowHeight(String sValue){
+		int vlen = 0; 
+		int numRows = 1 ;
+		StringTokenizer tok = new StringTokenizer(sValue);
+		while (tok.hasMoreTokens()){
+			int ctl = tok.nextToken().length() + 1;//Add Token length.
+			if ( vlen + ctl > numRows * MAX_COLUMN_LENGTH) {
+				vlen = (numRows * MAX_COLUMN_LENGTH) + ctl;
+				numRows ++ ;
+			}else {
+				vlen += ctl ;
+			}
+		}
+		return numRows ;
+	}
+	public static Font createDefaultFont(Workbook wb){
+		Font font = wb.createFont();
+		font.setFontName("Courier New");
+		font.setFontHeightInPoints((short)(CHARACTER_HEIGHT_IN_POINTS-2));
+		return font;
+	}
+	private void fixCellDimensions(Sheet sheet,Row row,Bucket columnNum,Object value){
+		int currentColumnWidth = getColumnWidth(sheet,columnNum.intValue());
+		String sValue = Database.getJdbcTypeHelper().getTypeRef(value.getClass()).getTypeConverter().toString(value);
+		int currentValueLength = sValue.length() ;
+		int maxRowHeight = getRowHeight(sValue);
+		
+		if (currentColumnWidth < MAX_COLUMN_LENGTH * CHARACTER_WIDTH){
+			int currentValueWidth = (currentValueLength + 1)* CHARACTER_WIDTH; 
+			currentColumnWidth = Math.min(Math.max(currentValueWidth,currentColumnWidth), MAX_COLUMN_LENGTH * CHARACTER_WIDTH);
+			sheet.setColumnWidth(columnNum.intValue(), currentColumnWidth);
+		}
+		row.setHeightInPoints(Math.max(row.getHeightInPoints() , maxRowHeight*CHARACTER_HEIGHT_IN_POINTS));
+		
+
+	}
+	public Cell createCell(Sheet sheet, Row row, Bucket columnNum , Object  value, CellStyle style){
+		Cell cell = row.createCell(columnNum.intValue());
+		cell.setCellStyle(style);
+		if (value != null){
+			Class<?> colClass = value.getClass();
+			if (style.getWrapText()){
+				fixCellDimensions(sheet, row, columnNum, value);
+			}
+			if (isNumeric(colClass)){
+				cell.setCellValue(Double.valueOf(String.valueOf(value)));
+			}else if (isDate(colClass)){
+				cell.setCellValue((Date)value);
+			}else if (isBoolean(colClass)){
+				cell.setCellValue(Boolean.valueOf(String.valueOf(value)));
+			}else {
+				cell.setCellValue(Database.getJdbcTypeHelper().getTypeRef(colClass).getTypeConverter().toString(value));
+			}
+		}
+		columnNum.increment();
+		return cell;
+	}
 }
