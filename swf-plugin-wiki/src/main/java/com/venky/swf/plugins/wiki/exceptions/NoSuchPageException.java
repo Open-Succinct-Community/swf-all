298cdf5d8b65673949b1665eb4f96d04fdedbd8e
Venky <venkatramnm@gmail.com>
2014-03-11 18:46:11 +0530

    move swf-plugin-wiki out of this repository to enable independent releases

--- swf-plugin-wiki/src/main/java/com/venky/swf/plugins/wiki/exceptions/NoSuchPageException.java
+++
@@ -1,8 +0,0 @@
-package com.venky.swf.plugins.wiki.exceptions;
-
-public class NoSuchPageException extends RuntimeException {
-	private static final long serialVersionUID = 6468231803459926553L;
-	public NoSuchPageException(){
-		super();
-	}
-}
