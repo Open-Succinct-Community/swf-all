a99216ec3bd43e534f5d42ca0f16a57df90319c8
Venky <venkatramnm@gmail.com>
2016-05-09 22:05:52 +0530

    Ensure Shutdown hook called to shut down the Delayed Task Daemon so that reset_router happens cleanly

--- swf/src/main/java/com/venky/swf/plugins/background/configuration/AppInstaller.java
+++ swf/src/main/java/com/venky/swf/plugins/background/configuration/AppInstaller.java
@@ -1,11 +1,12 @@
 package com.venky.swf.plugins.background.configuration;
 
 import com.venky.swf.configuration.Installer;
+import com.venky.swf.plugins.background.core.TaskManager;
 import com.venky.swf.plugins.background.core.workers.DelayedTaskManager;
 public class AppInstaller implements Installer{
 
   public void install() {
-    DelayedTaskManager.instance(); //Bring up the daemon
+    TaskManager.instance().wakeUp(); //Bring up the daemon
   }
 }
 
