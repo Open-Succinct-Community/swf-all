52fe4f53d76dc543fc52bc3dac99c62d1c024f5d
Venky <venkatramanm@gmail.com>
2019-02-21 15:21:55 +0530

    V4 Cache  Server Changes

--- swf-db/src/main/java/com/venky/swf/db/table/ModelInvocationHandler.java
+++ swf-db/src/main/java/com/venky/swf/db/table/ModelInvocationHandler.java
@@ -52,7 +52,7 @@ public class ModelInvocationHandler implements InvocationHandler {
     private List<String> virtualFields = new IgnoreCaseList(false);
     private String modelName = null;
 
-    private Model proxy = null;
+    private transient Model proxy = null;
     private transient ModelReflector<? extends Model> reflector = null;
 
 	@SuppressWarnings("unchecked")
@@ -355,11 +355,11 @@ public class ModelInvocationHandler implements InvocationHandler {
 	private <M extends Model> void bootStrapProxy(M m) {
 		if (proxy == null) {
 	    	setProxy(m);
-	    	if (modelImplObjects.isEmpty()){
-				List<Class<?>> modelImplClasses = getModelImplClasses(modelClass);
-				for (Class<?> implClass: modelImplClasses){
-					addModelImplObject(constructImpl(implClass, m));
-				}
+		}
+		if (modelImplObjects.isEmpty()){
+			List<Class<?>> modelImplClasses = getModelImplClasses(modelClass);
+			for (Class<?> implClass: modelImplClasses){
+				addModelImplObject(constructImpl(implClass, m));
 			}
 		}
 	}
