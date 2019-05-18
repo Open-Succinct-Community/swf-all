f3f0adee374622a13e7b46b1a65e7dff72728a08
Venky <venkatramanm@gmail.com>
2019-05-18 21:19:37 +0530

    Minor Fixes

--- swf-db/src/main/java/com/venky/swf/db/model/io/AbstractModelReader.java
+++ swf-db/src/main/java/com/venky/swf/db/model/io/AbstractModelReader.java
@@ -48,7 +48,7 @@ public abstract class AbstractModelReader<M extends Model,T> extends ModelIO<M> 
 				Model referredModel = reader.read(refElement);
 				if (referredModel != null){
 					if (referredModel.getRawRecord().isNewRecord()) {
-						throw new RuntimeException(referredModelClass.getSimpleName() + " passed is not associated with " +  getReflector().getModelClass().getSimpleName());
+						throw new RuntimeException(referredModelClass.getSimpleName() + " passed does not exist " );
 					}
 					getReflector().set(m, getReflector().getReferenceField(referredModelGetter), referredModel.getId());
 				}else {
