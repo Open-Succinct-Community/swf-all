29b4c4570ff7f56396e92810e63cbd0e0f96f9c7
Venky <venkatramanm@gmail.com>
2019-06-27 15:29:04 +0530

    Cache

--- swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/participants/admin/Facility.java
+++ swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/participants/admin/Facility.java
@@ -8,6 +8,7 @@ import com.venky.swf.db.annotations.column.UNIQUE_KEY;
 import com.venky.swf.db.annotations.column.pm.PARTICIPANT;
 import com.venky.swf.db.annotations.column.ui.OnLookupSelect;
 import com.venky.swf.db.annotations.column.validations.RegEx;
+import com.venky.swf.db.annotations.model.CONFIGURATION;
 import com.venky.swf.db.model.Model;
 import com.venky.swf.plugins.collab.db.model.CompanySpecific;
 import com.venky.swf.plugins.collab.db.model.config.City;
@@ -15,6 +16,7 @@ import com.venky.swf.plugins.collab.db.model.config.Country;
 import com.venky.swf.plugins.collab.db.model.config.State;
 
 
+@CONFIGURATION
 public interface Facility extends CompanySpecific, Address , Model{
 	@UNIQUE_KEY
 	public String getName();
