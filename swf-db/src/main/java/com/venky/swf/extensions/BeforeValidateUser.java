36444c1396521574644d5c3da4029e7a8bcd0cd4
Venky <venkatramanm@gmail.com>
2019-07-09 13:38:09 +0530

    Password Encryption fixes

--- swf-db/src/main/java/com/venky/swf/extensions/BeforeValidateUser.java
+++ swf-db/src/main/java/com/venky/swf/extensions/BeforeValidateUser.java
@@ -4,7 +4,13 @@ import com.venky.core.util.ObjectUtil;
 import com.venky.digest.Encryptor;
 import com.venky.swf.db.extensions.BeforeModelValidateExtension;
 import com.venky.swf.db.model.User;
+import com.venky.swf.db.model.reflection.ModelReflector;
 import com.venky.swf.routing.Config;
+import com.venky.swf.sql.Expression;
+import com.venky.swf.sql.Operator;
+import com.venky.swf.sql.Select;
+
+import java.util.List;
 
 public class BeforeValidateUser extends BeforeModelValidateExtension<User>{
 	static {
@@ -13,17 +19,28 @@ public class BeforeValidateUser extends BeforeModelValidateExtension<User>{
 	
 	@Override
 	public void beforeValidate(User model) {
-		if (ObjectUtil.isVoid(model.getLongName())){
-			if (!ObjectUtil.isVoid(model.getName())){
-				String name = model.getName();
-				int at_position =  name.indexOf('@');
-				if (at_position >= 0){
-					name = name.substring(0,at_position);  //Avoid Special Characters of email id
+		if (!ObjectUtil.isVoid(model.getName())) {
+			String name = model.getName();
+			if (ObjectUtil.isVoid(model.getLongName())) {
+				int at_position = name.indexOf('@');
+				if (at_position >= 0) {
+					name = name.substring(0, at_position);  //Avoid Special Characters of email id
 				}
-
 				model.setLongName(name);
 			}
+			if (model.getRawRecord().isNewRecord()){
+				List<User> userList = new Select().from(User.class).where(new Expression(ModelReflector.instance(User.class).getPool(),"lower(NAME)", Operator.EQ, name.toLowerCase())).execute();
+				if (!userList.isEmpty()){
+					if (name.indexOf('@') > 0){
+						throw new RuntimeException("Email already registered!");
+					}else {
+						throw new RuntimeException("Login Id already registered!");
+					}
+				}
+			}
 		}
+
+
 		if (Config.instance().shouldPasswordsBeEncrypted()){
 			if (model.getReflector().isVoid(model.getCreatedAt())){
 				model.setCreatedAt(model.getReflector().getNow());
--- swf-plugin-mail/src/main/java/com/venky/swf/plugins/mail/db/model/User.java
+++ swf-plugin-mail/src/main/java/com/venky/swf/plugins/mail/db/model/User.java
@@ -3,6 +3,7 @@ package com.venky.swf.plugins.mail.db.model;
 import java.util.List;
 
 import com.venky.swf.db.annotations.column.relationship.CONNECTED_VIA;
+import com.venky.swf.db.annotations.column.ui.HIDDEN;
 
 
 public interface User extends com.venky.swf.db.model.User{
@@ -10,6 +11,7 @@ public interface User extends com.venky.swf.db.model.User{
 	public void sendMail(String subject, String text);
 
 	@CONNECTED_VIA("USER_ID")
+	@HIDDEN
 	public List<Mail> getSentMails();
 
 
