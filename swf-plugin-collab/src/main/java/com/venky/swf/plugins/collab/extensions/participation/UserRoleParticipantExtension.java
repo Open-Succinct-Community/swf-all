871563f2f80501676bb987bc528d4f84905fe41a
Venky <venkatramanm@gmail.com>
2019-09-23 20:07:24 +0530

    User Email Participant Extension Fixed

--- swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/extensions/participation/UserRoleParticipantExtension.java
+++ swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/extensions/participation/UserRoleParticipantExtension.java
@@ -2,6 +2,7 @@ package com.venky.swf.plugins.collab.extensions.participation;
 
 import java.util.List;
 
+import com.venky.core.collections.SequenceSet;
 import com.venky.swf.db.extensions.ParticipantExtension;
 import com.venky.swf.db.model.User;
 import com.venky.swf.plugins.security.db.model.UserRole;
@@ -16,9 +17,18 @@ public class UserRoleParticipantExtension extends ParticipantExtension<UserRole>
 	@Override
 	protected List<Long> getAllowedFieldValues(User user,
 			UserRole partiallyFilledModel, String fieldName) {
+		SequenceSet<Long> ret = null;
 		if (fieldName.equalsIgnoreCase("USER_ID")){
-			return DataSecurityFilter.getIds(DataSecurityFilter.getRecordsAccessible(User.class, user));
+			if (!partiallyFilledModel.getReflector().isVoid(partiallyFilledModel.getUserId())){
+				ret = new SequenceSet<>();
+				if (partiallyFilledModel.getUserId() == user.getId() || partiallyFilledModel.getUser().isAccessibleBy(user)) {
+					ret.add(partiallyFilledModel.getUserId());
+				}
+			}else if (!user.getRawRecord().getAsProxy(com.venky.swf.plugins.collab.db.model.user.User.class).isStaff()){
+				ret = DataSecurityFilter.getIds(DataSecurityFilter.getRecordsAccessible(com.venky.swf.plugins.collab.db.model.user.User.class, user));
+			}
 		}
-		return null;
+		return ret;
+
 	}
 }
