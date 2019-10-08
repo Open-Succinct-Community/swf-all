c103f4e2c2c8644fc38573651b306bee70d5ce27
Venky <venkatramanm@gmail.com>
2019-10-08 18:10:02 +0530

    PWA templates

--- swf-archetype/src/main/resources/archetype-resources/src/main/resources/web_manifest/manifest.js
+++ swf-archetype/src/main/resources/archetype-resources/src/main/resources/web_manifest/manifest.js
@@ -1,2 +1,12 @@
 self.addEventListener('fetch', function(event){
 });
+
+var deferredPrompt;
+self.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
+});
