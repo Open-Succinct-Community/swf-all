96822765e38129739fa0b406586ad8c443c5220f
Venky <venkatramanm@gmail.com>
2019-11-07 23:18:02 +0530

    Phone number  sanitization changes to support internation numbers if they start with +

--- swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/user/Phone.java
+++ swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/user/Phone.java
@@ -4,6 +4,7 @@ import com.venky.core.util.ObjectUtil;
 import com.venky.swf.db.annotations.column.IS_NULLABLE;
 import com.venky.swf.db.annotations.column.ui.WATERMARK;
 
+import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 public interface Phone extends OtpEnabled{
@@ -13,22 +14,38 @@ public interface Phone extends OtpEnabled{
     public void setPhoneNumber(String phoneNumber);
 
     public static String sanitizePhoneNumber(String phoneNumber){
+        if (ObjectUtil.isVoid(phoneNumber)) {
+            return  "";
+        }
 
-        if (!ObjectUtil.isVoid(phoneNumber)){
-            int length = phoneNumber.length();
+        StringBuilder ret = new StringBuilder();
+        StringTokenizer tokenizer = new StringTokenizer(phoneNumber,"\r\f\t\n -+:()");
+        while (tokenizer.hasMoreTokens()){
+            ret.append(tokenizer.nextToken());
+        }
+        while (ret.charAt(0) == '0'){
+            ret.deleteCharAt(0);
+        }
+        if (phoneNumber.charAt(0) == '+'){
+            ret.insert(0,'+');
+        }else {
+            int length = ret.length();
             if (length == 10){
-                phoneNumber = ("+91"+phoneNumber);
+                ret.insert(0,"+91");
             }else if (length == 12){
-                phoneNumber = ("+"+phoneNumber);
+                ret.insert(0,"+");
             }
-            if (phoneNumber.length() != 13){
-                throw new RuntimeException("Phone number invalid e.g. +911234567890");
-            }
-            Pattern pattern = Pattern.compile("\\+[0-9]+");
-            if (!pattern.matcher(phoneNumber).matches()){
+
+            if (ret.length() != 13){
                 throw new RuntimeException("Phone number invalid e.g. +911234567890");
             }
         }
-        return phoneNumber;
+
+        Pattern pattern = Pattern.compile("\\+[0-9]+");
+        if (!pattern.matcher(ret.toString()).matches()){
+            throw new RuntimeException("Phone number invalid e.g. +911234567890");
+        }
+
+        return ret.toString();
     }
 }
