04db7b8a75a17d6275880fb6c1812e61631b3eb0
Venky <venkatramanm@gmail.com>
2020-04-09 22:38:52 +0530

    Participant Extension clean up

--- swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/participants/admin/CompanyImpl.java
+++ swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/participants/admin/CompanyImpl.java
@@ -12,6 +12,8 @@ import com.venky.swf.sql.Operator;
 import com.venky.swf.sql.Select;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CompanyImpl extends ModelImpl<Company>{
 
@@ -25,16 +27,38 @@ public class CompanyImpl extends ModelImpl<Company>{
 
 
 	public List<Long> getStaffUserIds(){
+		ModelReflector<Role> roleRef = ModelReflector.instance(Role.class);
+		List<Role> staffRoles = new Select().from(Role.class).where(new Expression(roleRef.getPool(),Conjunction.OR).
+				add(new Expression(roleRef.getPool(),"NAME",Operator.EQ,"STAFF")).
+				add(new Expression(roleRef.getPool(),"STAFF",Operator.EQ,true))).execute();
+
+
+
+
+
+		ModelReflector<UserRole> userRoleModelReflector = ModelReflector.instance(UserRole.class);
+		List<UserRole> staffUsers = new Select().from(UserRole.class).where(
+				new Expression(userRoleModelReflector.getPool(),"ROLE_ID",Operator.EQ, DataSecurityFilter.getIds(staffRoles))
+		).execute();
+
+		Set<Long> userIds = staffUsers.stream().map(su->su.getUserId()).collect(Collectors.toSet());
+
+
 		Company company = getProxy();
+
 		ModelReflector<User> ref = ModelReflector.instance(User.class);
-		Expression where = new Expression(ref.getPool(), "COMPANY_ID", Operator.EQ, company.getId());
+		Expression where = new Expression(ref.getPool(),Conjunction.AND);
+		where.add(new Expression(ref.getPool(), "COMPANY_ID", Operator.EQ, company.getId()));
+		where.add(new Expression(ref.getPool(),"USER_ID",Operator.IN,userIds.toArray()));
+
 
 		Select select = new Select("ID").from(User.class).where(where);
 
+		/*
 		select.add(" and exists (select 1 from user_roles , roles where user_roles.user_id = users.id " +
 						" and  roles.id = user_roles.role_id " +
 						" and ( roles.name = 'STAFF' or roles.staff = true ) )" );
-
+		*/
 		List<User> users = select.execute();
 		return DataSecurityFilter.getIds(users);
 	}
--- swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/user/User.java
+++ swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/user/User.java
@@ -4,6 +4,7 @@ import com.venky.swf.db.annotations.column.COLUMN_NAME;
 import com.venky.swf.db.annotations.column.HOUSEKEEPING;
 import com.venky.swf.db.annotations.column.IS_VIRTUAL;
 import com.venky.swf.db.annotations.column.pm.PARTICIPANT;
+import com.venky.swf.db.annotations.column.relationship.CONNECTED_VIA;
 import com.venky.swf.db.annotations.column.ui.HIDDEN;
 import com.venky.swf.db.annotations.column.ui.PROTECTION;
 import com.venky.swf.plugins.collab.db.model.CompanyNonSpecific;
@@ -28,7 +29,10 @@ public interface User extends com.venky.swf.plugins.templates.db.model.User, Add
 	@IS_VIRTUAL
 	public User getSelfUser();
 
+	@CONNECTED_VIA("USER_ID")
 	public List<UserFacility> getUserFacilities();
+
+	@CONNECTED_VIA("USER_ID")
 	public List<UserPhone> getUserPhones();
 
 
