0cadddf0065734933af78f78f194582e236f4a9a
Venky <venkatramanm@gmail.com>
2020-04-18 15:33:38 +0530

    Relation ship pruned

--- swf-plugin-templates/src/main/java/com/venky/swf/plugins/templates/db/model/User.java
+++ swf-plugin-templates/src/main/java/com/venky/swf/plugins/templates/db/model/User.java
@@ -3,6 +3,7 @@ package com.venky.swf.plugins.templates.db.model;
 
 import com.venky.swf.db.annotations.column.COLUMN_DEF;
 import com.venky.swf.db.annotations.column.defaulting.StandardDefault;
+import com.venky.swf.db.annotations.column.relationship.CONNECTED_VIA;
 import com.venky.swf.db.annotations.column.ui.HIDDEN;
 import com.venky.swf.plugins.templates.db.model.alerts.Alert;
 import com.venky.swf.plugins.templates.db.model.alerts.Device;
@@ -11,6 +12,7 @@ import java.util.List;
 
 public interface User extends com.venky.swf.plugins.mail.db.model.User {
 
+    @CONNECTED_VIA("USER_ID")
     public List<Alert> getAlerts();
 
     @HIDDEN
