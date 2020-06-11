0757340bed655b658f6640924bed87a50c97972c
Venky <venkatramanm@gmail.com>
2020-06-11 17:02:26 +0530

    Unique key of index directory

--- swf/src/main/java/com/venky/swf/plugins/lucene/db/model/IndexDirectory.java
+++ swf/src/main/java/com/venky/swf/plugins/lucene/db/model/IndexDirectory.java
@@ -2,12 +2,15 @@ package com.venky.swf.plugins.lucene.db.model;
 
 import java.util.List;
 
+import com.venky.swf.db.annotations.column.UNIQUE_KEY;
+import com.venky.swf.db.annotations.column.indexing.Index;
 import com.venky.swf.db.annotations.model.CONFIGURATION;
 import com.venky.swf.db.annotations.model.EXPORTABLE;
 import com.venky.swf.db.model.Model;
 
 @EXPORTABLE(false)
 public interface IndexDirectory extends Model{
+	@UNIQUE_KEY
 	public String getName();
 	public void setName(String tableName);
 	
