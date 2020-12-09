ffbdcbd0b9aefa64c75160857b63a56a3d585d95
Venky <venkatramanm@gmail.com>
2020-12-10 02:32:00 +0530

    KeepAlive session even on login with json

--- swf/src/main/java/com/venky/swf/path/Path.java
+++ swf/src/main/java/com/venky/swf/path/Path.java
@@ -659,7 +659,7 @@ public class Path implements _IPath{
         if (getProtocol() != MimeType.TEXT_HTML) {
             adaptor = IntegrationAdaptor.instance(User.class, FormatHelper.getFormatClass(getProtocol()));
         }
-        autoInvalidate = adaptor != null || !ObjectUtil.isVoid(getHeader("ApiKey"));
+        autoInvalidate = ( adaptor != null && !Database.getJdbcTypeHelper("").getTypeRef(Boolean.class).getTypeConverter().valueOf(getHeader("KeepAlive")) ) || !ObjectUtil.isVoid(getHeader("ApiKey"));
         if (user == null){
             if (getRequest().getMethod().equalsIgnoreCase("POST")){
                 String username = null;
