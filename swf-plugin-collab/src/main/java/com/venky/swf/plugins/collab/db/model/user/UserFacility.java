5ec8e0a027fda8637975818a5b3c6b44a24fa3f1
Venky <venkatramanm@gmail.com>
2020-12-17 16:12:34 +0530

    User facility index

--- swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/user/UserFacility.java
+++ swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/user/UserFacility.java
@@ -1,6 +1,7 @@
 package com.venky.swf.plugins.collab.db.model.user;
 
 import com.venky.swf.db.annotations.column.UNIQUE_KEY;
+import com.venky.swf.db.annotations.column.indexing.Index;
 import com.venky.swf.db.annotations.column.pm.PARTICIPANT;
 import com.venky.swf.db.annotations.column.ui.HIDDEN;
 import com.venky.swf.db.model.Model;
@@ -9,12 +10,14 @@ import com.venky.swf.plugins.collab.db.model.participants.admin.Facility;
 public interface UserFacility extends Model {
 	@PARTICIPANT
 	@UNIQUE_KEY
+	@Index
 	public long getUserId();
 	public void setUserId(long id);
 	public User getUser();
 	
 	@PARTICIPANT
 	@UNIQUE_KEY
+	@Index
 	public long getFacilityId();
 	public void setFacilityId(long id);
 	public Facility getFacility();
