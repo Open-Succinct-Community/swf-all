a6b82663ee4f870d7fc8b5af129db42df9a0c22c
Venky <venkatramanm@gmail.com>
2021-04-01 00:40:15 +0530

    Index Alert

--- swf-plugin-templates/src/main/java/com/venky/swf/plugins/templates/db/model/alerts/Alert.java
+++ swf-plugin-templates/src/main/java/com/venky/swf/plugins/templates/db/model/alerts/Alert.java
@@ -3,6 +3,7 @@ package com.venky.swf.plugins.templates.db.model.alerts;
 import com.venky.swf.db.annotations.column.COLUMN_SIZE;
 import com.venky.swf.db.annotations.column.IS_NULLABLE;
 import com.venky.swf.db.annotations.column.UNIQUE_KEY;
+import com.venky.swf.db.annotations.column.indexing.Index;
 import com.venky.swf.db.annotations.column.pm.PARTICIPANT;
 import com.venky.swf.db.annotations.column.ui.HIDDEN;
 import com.venky.swf.db.annotations.model.EXPORTABLE;
@@ -18,14 +19,17 @@ public interface Alert  extends Model  {
 
     @PARTICIPANT
     @IS_NULLABLE(false)
+    @Index
     public Long getUserId();
     public void setUserId(Long userId);
     public User getUser();
 
+    @Index
     public String getSubject();
     public void setSubject(String subject);
 
     @COLUMN_SIZE(512)
+    @Index
     public String getMessage();
     public void setMessage(String message);
 
