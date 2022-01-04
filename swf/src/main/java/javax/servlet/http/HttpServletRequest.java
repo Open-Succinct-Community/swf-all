570cb68629a541c613101190b058d4d4c8cef4c0
Venky <venkatramanm@gmail.com>
2022-01-05 00:28:34 +0530

    javax issue

--- swf/src/main/java/javax/servlet/http/HttpServletRequest.java
+++ swf/src/main/java/javax/servlet/http/HttpServletRequest.java
@@ -1,8 +1,11 @@
 package javax.servlet.http;
 
+
+import javax.servlet.ServletInputStream;
 import javax.servlet.ServletRequest;
+import java.io.IOException;
 
 public interface HttpServletRequest extends jakarta.servlet.http.HttpServletRequest, ServletRequest {
-
-
+    @Override
+    ServletInputStream getInputStream() throws IOException;
 }
