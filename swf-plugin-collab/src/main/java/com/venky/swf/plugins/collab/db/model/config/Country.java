dc04c075efaa88e3693b957bd04506a9b2f0e25e
Venky <venkatramanm@gmail.com>
2022-12-10 23:32:35 +0530

    find method fallback

--- swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/config/Country.java
+++ swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/db/model/config/Country.java
@@ -2,12 +2,14 @@ package com.venky.swf.plugins.collab.db.model.config;
 
 import java.util.List;
 
+import com.openhtmltopdf.css.parser.property.CounterPropertyBuilder.CounterReset;
 import com.venky.swf.db.Database;
 import com.venky.swf.db.annotations.column.UNIQUE_KEY;
 import com.venky.swf.db.annotations.column.indexing.Index;
 import com.venky.swf.db.annotations.model.CONFIGURATION;
 import com.venky.swf.db.annotations.model.EXPORTABLE;
 import com.venky.swf.db.model.Model;
+import com.venky.swf.path._IPath;
 import com.venky.swf.sql.Conjunction;
 import com.venky.swf.sql.Expression;
 import com.venky.swf.sql.Operator;
@@ -35,14 +37,17 @@ public interface Country extends Model{
 	public List<State> getStates(); 
 	
 	public static Country findByName(String name) { 
-		Select s = new Select().from(Country.class); 
+		Select s = new Select().from(Country.class);
 		s.where(new Expression(s.getPool(),"lower(NAME)",Operator.EQ,name.toLowerCase()));
-		List<Country> country = s.execute(); 
-		if (country.size() == 1) {
-			return country.get(0);
-		}else {
-			throw new RuntimeException("Country " + name + " not found!");
+		List<Country> countries = s.execute();
+		Country country = null;
+		if (countries.size() == 1) {
+			country = countries.get(0);
 		}
+		if (country == null){
+			country = findByISO(name);
+		}
+        return country;
 	}
 	public static Country findByISO(String isoCode) { 
 		Select s = new Select().from(Country.class); 
