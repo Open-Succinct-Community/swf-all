21987d93fcecf3b3cd92579da1e547edb9d14fc0
Venky <venkatramanm@gmail.com>
2024-07-17 13:04:48 +0530

    +91 addition causes uk violation

--- swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/extensions/beforesave/BeforeValidatePhone.java
+++ swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/extensions/beforesave/BeforeValidatePhone.java
@@ -1,5 +1,7 @@
 package com.venky.swf.plugins.collab.extensions.beforesave;
 
+import com.venky.core.util.ObjectUtil;
+import com.venky.swf.db.Database;
 import com.venky.swf.db.extensions.BeforeModelValidateExtension;
 import com.venky.swf.db.model.Model;
 import com.venky.swf.plugins.collab.db.model.user.Phone;
@@ -8,5 +10,13 @@ public class BeforeValidatePhone<P extends Phone& Model> extends BeforeModelVali
     @Override
     public void beforeValidate(P model) {
         model.setPhoneNumber(Phone.sanitizePhoneNumber(model.getPhoneNumber()));
+        if (!ObjectUtil.isVoid(model.getPhoneNumber())){
+            if (model.getRawRecord().isNewRecord()){
+                P p = Database.getTable(getModelClass()).getRefreshed(model,false);
+                if (!p.getRawRecord().isNewRecord()){
+                    model.setRawRecord(p.getRawRecord());
+                }
+            }
+        }
     }
 }
