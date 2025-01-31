2af0103ce3b35d610ce02f07808dd53bc7d53f20
Venky <venkatramanm@gmail.com>
2025-01-31 17:06:55 +0530

    Set Email if void and name matches email pattern

--- swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/extensions/beforesave/BeforeValidateUser.java
+++ swf-plugin-collab/src/main/java/com/venky/swf/plugins/collab/extensions/beforesave/BeforeValidateUser.java
@@ -4,11 +4,27 @@ import com.venky.core.util.ObjectUtil;
 import com.venky.swf.db.extensions.BeforeModelValidateExtension;
 import com.venky.swf.db.model.reflection.ModelReflector;
 import com.venky.swf.plugins.collab.db.model.user.User;
+import org.hazlewood.connor.bottema.emailaddress.EmailAddressValidator;
 
 import java.util.regex.Pattern;
 
 public class BeforeValidateUser extends BeforeValidateAddress<User> {
     static {
         registerExtension(new BeforeValidateUser());
+    }
+    
+    @Override
+    public void beforeValidate(User user) {
+        if (!ObjectUtil.isVoid(user.getName()) && user.getName().contains("@")){
+            if (EmailAddressValidator.isValid(user.getName())) {
+                if (ObjectUtil.isVoid(user.getEmail()) ||
+                        !ObjectUtil.equals(user.getName(),user.getEmail())) {
+                    user.setEmail(user.getName().toLowerCase());
+                }
+            }else {
+                throw new RuntimeException("Please enter a valid email address!");
+            }
+        }
+        super.beforeValidate(user);
     }
 }
